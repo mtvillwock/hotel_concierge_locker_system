bbfe4f0b17def0c0d5417fb9d85b056f4008d2a7
Matthew Villwock <mtvillwock@gmail.com>
2015-04-10 10:52:13 -0700

    remove locker controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'lockers/store_bag'
-
-  get 'lockers/retrieve_bag'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-    root 'lockers#index'
   # root 'welcome#index'
 
   # Example of regular route:
