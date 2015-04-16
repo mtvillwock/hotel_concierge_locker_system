89e99c67f1f415c14ca97cf30af5127f28ed9c81
Matthew Villwock <mtvillwock@gmail.com>
2015-04-16 12:07:47 -0700

    set up skeleton for writing BagsController tests

--- spec/controllers/bags_controller.rb
+++ spec/controllers/bags_controller.rb
@@ -6,48 +6,64 @@ RSpec.describe BagsController, :type => :controller do
   let!(:locker) { create(:locker) }
   let!(:ticket) { create(:ticket) }
 
-  it "creates an answer with correct params" do
-    expect {
-      post :create, question_id: question, answer: { username: answer.username, body: answer.body }
-    }.to change{
-      question.answers.count
-    }.by(1)
+  context '#index' do
+    xit '' do
+
+    end
+
+    xit '' do
+
+    end
   end
 
-  it "doesn't create an answer with nobody. so lonely" do
-    answer = {username: "Skeeeltor"}
-    expect {
-      post :create, question_id: question, answer: answer
-    }.to_not change{
-      question.answers.count
-    }
+  context '#show' do
+    xit '' do
+
+    end
+    xit '' do
+
+    end
   end
 
-  it "doesn't create an answer with no username" do
-    answer = {body: "Hey there, you mighty lookin fin."}
-    expect {
-      post :create, question_id: question, answer: answer
-    }.to_not change{
-      question.answers.count
-    }
+  context '#new' do
+    xit '' do
+
+    end
+    xit '' do
+
+    end
   end
 
-  it "updates an answer" do
-    attributes = { username: "I will update you", body: "Updated"}
-    answer.question = create(:question)
-    answer.save
-    expect {
-      put :update, question_id: answer.question, id: answer.id, answer: attributes
-    }.to change {
-      answer.reload.body
-    }
+  context '#create' do
+    xit '' do
+      expect {
+        post :create, bag: { size: bag.size }
+      }.to change{
+        bags.count
+      }.by(1)
+    end
+    xit '' do
+
+    end
+
   end
 
-  it "destroys a answer" do
-    expect {
-      delete :destroy, question_id: question,  :id => answer.id
-      }.to change(Answer, :count).by(-1)
+  context '#edit' do
+    xit '' do
+
+    end
+    xit '' do
+
+    end
+
   end
 
+  context '#update' do
+    xit '' do
+
+    end
+    xit '' do
+
+    end
+  end
 end
-
