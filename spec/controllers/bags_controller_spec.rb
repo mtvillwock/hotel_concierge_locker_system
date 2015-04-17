143323265a40e046061e293f3e452327a3d1ca97
Matthew Villwock <mtvillwock@gmail.com>
2015-04-17 16:43:37 -0700

    added example from Relishapp

--- spec/controllers/bags_controller_spec.rb
+++ spec/controllers/bags_controller_spec.rb
@@ -7,7 +7,40 @@ RSpec.describe BagsController, :type => :controller do
   let!(:locker) { FactoryGirl.build :locker }
   let!(:ticket) { FactoryGirl.build :ticket }
 
+  describe "GET index" do
+    it "has a 200 status code" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+
+
+  #   RSpec.describe "Widget management", :type => :request do
+
+  #   it "creates a Widget and redirects to the Widget's page" do
+  #     get "/widgets/new"
+  #     expect(response).to render_template(:new)
+
+  #     post "/widgets", :widget => {:name => "My Widget"}
+
+  #     expect(response).to redirect_to(assigns(:widget))
+  #     follow_redirect!
+
+  #     expect(response).to render_template(:show)
+  #     expect(response.body).to include("Widget was successfully created.")
+  #   end
+
+  # end
+
   context '#create' do
+    it 'has a 302 status code' do
+      post :create, bag: { size: %w(small medium large) }
+      p response
+      follow_redirect!
+      expect(last_response).to redirect_to(assigns(:bag))
+      # expect(response.status).to eq(302)
+      # this is returning 200
+    end
     it 'creates a bag, stores it in a locker, and creates a ticket' do
       # Is there a proper way to extract this into multiple tests?
       p bag
@@ -26,9 +59,9 @@ RSpec.describe BagsController, :type => :controller do
       expect(Bag.count).to be(count + 1)
     end
     # it 'redirects user to the /bags/:id #show route' do
-      # response = post(:create, bag: { size: bag.size })
-      # p response
-      # expect(response).to redirect_to("/bags/#{bag.id}")
+    # response = post(:create, bag: { size: bag.size })
+    # p response
+    # expect(response).to redirect_to("/bags/#{bag.id}")
     # end
   end
 end
