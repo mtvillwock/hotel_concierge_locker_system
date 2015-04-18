870ad5c6540405795c80a765dcc499b51be9381e
Matthew Villwock <mtvillwock@gmail.com>
2015-04-17 18:57:20 -0700

    refactor ticket spec with factory girl methods, 7 tests passing

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -6,30 +6,27 @@ describe Ticket do
     it { should validate_presence_of :bag_id }
   end
 
-  context 'valid Ticket'
-  it 'is valid with a bag id and locker id' do
-    ticket = Ticket.new(locker_id: 1, bag_id: 1)
-    expect(ticket).to be_valid
+  context 'valid Ticket' do
+    it 'is valid with a bag id and locker id' do
+      expect(build(:ticket, locker_id:1, bag_id: 1)).to be_valid
+    end
   end
+
   context 'invalid Ticket' do
     it 'is invalid with non-integer locker id' do
-      ticket = Ticket.new(locker_id: "not an integer", bag_id: 1)
-      expect(ticket).to be_invalid
+      expect(build(:ticket, locker_id: "not a number", bag_id: 1)).to be_invalid
     end
 
     it 'is invalid with no locker id' do
-      ticket = Ticket.new(locker_id: nil, bag_id: 1)
-      expect(ticket).to be_invalid
+      expect(build(:ticket, locker_id: nil, bag_id: 1)).to be_invalid
     end
 
     it 'is invalid with non-integer bag id' do
-      ticket = Ticket.new(locker_id: 1, bag_id: "not an integer")
-      expect(ticket).to be_invalid
+      expect(build(:ticket, locker_id: 1, bag_id: "not a number")).to be_invalid
     end
 
     it 'is invalid with no bag id' do
-      ticket = Ticket.new(locker_id: 1, bag_id: nil)
-      expect(ticket).to be_invalid
+      expect(build(:ticket, locker_id: 1, bag_id: nil)).to be_invalid
     end
   end
 end
