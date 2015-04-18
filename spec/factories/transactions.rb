1406ca94b43189c6098ef833c7561b8dffd87817
Matthew Villwock <mtvillwock@gmail.com>
2015-04-18 02:23:41 -0700

    write transaction factories and use them to write spec tests for transaction model

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,6 +1,25 @@
 FactoryGirl.define do
   factory :transaction do
-    
-  end
+    factory :valid_transaction do
+      bag
+      locker
+      ticket
+    end
 
+    factory :invalid_transaction do
+      # bag nil
+      # locker nil
+      # ticket nil
+      # should this just be a single invalid transaction?
+      factory :transaction_without_bag do
+        bag nil
+      end
+      factory :transaction_without_locker do
+        locker nil
+      end
+      factory :transaction_without_ticket do
+        ticket nil
+      end
+    end
+  end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describeTransaction do
+    context 'associations' do
+      it { should have_one(:bag) }
+      it { should have_one(:locker) }
+      it { should have_one(:ticket) }
+    end
+
+    context 'valid Transaction' do
+      expect(build(:valid_transaction)).to be_valid
+    end
+
+    context 'invalid Transaction' do
+      it 'has no bag' do
+        expect(build(:transaction_without_bag)).to be_invalid
+      end
+      it 'has no locker' do
+        expect(build(:transaction_without_locker)).to be_invalid
+      end
+      it 'has no ticket' do
+        expect(build(:transaction_without_ticket)).to be_invalid
+      end
+    end
+  end
