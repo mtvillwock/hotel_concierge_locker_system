ce77094e7b94a787314dade6df4e35809c1848f7
Matthew Villwock <mtvillwock@gmail.com>
2015-04-17 19:15:18 -0700

    write skeleton for bags controller tests

--- spec/controllers/bags_controller_spec.rb
+++ spec/controllers/bags_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe BagsController, :type => :controller do
   # let!(:locker) { FactoryGirl.build :locker }
   # let!(:ticket) { FactoryGirl.build :ticket }
 
-  context "GET index" do
+  describe "GET #index" do
     it "has a 200 status code" do
       get :index
       expect(response.status).to eq(200)
@@ -16,54 +16,53 @@ RSpec.describe BagsController, :type => :controller do
     it 'displays all bags currently in lockers'
   end
 
+  describe 'GET #show' do
+    it 'assigns the requested bag to @bag'
+    it 'renders the :show template'
+  end
 
-  #   RSpec.describe "Widget management", :type => :request do
+  describe 'GET #new' do
+    it 'assigns a new Bag to @bag'
+    it 'renders the :new template'
+  end
 
-  #   it "creates a Widget and redirects to the Widget's page" do
-  #     get "/widgets/new"
-  #     expect(response).to render_template(:new)
+  describe 'GET #edit' do
+    it 'assigns the requested bag to @bag'
+    it 'renders the :edit template'
+  end
 
-  #     post "/widgets", :widget => {:name => "My Widget"}
+  describe 'POST #create' do
+    context "with valid attributes" do
+      it 'saves the new bag in the database' do
+        # Is there a proper way to extract this into multiple tests?
+        p bag
+        p locker
+        p ticket
+        count = Bag.count
+        post(:create, bag: { size: bag.size })
+        locker = Locker.new
+        locker.current_bag = bag
+        locker.empty = false
+        locker.save
+        ticket = Ticket.new(locker_id: locker.id, bag_id: bag.id)
+        p bag
+        p locker
+        p ticket
+        expect(Bag.count).to be(count + 1)
+      end
 
-  #     expect(response).to redirect_to(assigns(:widget))
-  #     follow_redirect!
-
-  #     expect(response).to render_template(:show)
-  #     expect(response.body).to include("Widget was successfully created.")
-  #   end
-
-  # end
-
-  context '#create' do
-    it "redirects to a bag's page upon save" do
-      size = %w(small medium large).sample
-      # should this be broken out to cover each size explicitly?
-      post :create, bag: { size: size }
-      expect(response).to redirect_to(assigns(:bag))
-      # expect(response.status).to eq(302)
-      # this is returning 200
+      it "redirects to bags#show" do
+        size = %w(small medium large).sample
+        # should this be broken out to cover each size explicitly?
+        post :create, bag: { size: size }
+        expect(response).to redirect_to(assigns(:bag))
+        # expect(response.status).to eq(302)
+        # this is returning 200
+      end
     end
-    it 'creates a bag, stores it in a locker, and creates a ticket' do
-      # Is there a proper way to extract this into multiple tests?
-      p bag
-      p locker
-      p ticket
-      count = Bag.count
-      post(:create, bag: { size: bag.size })
-      locker = Locker.new
-      locker.current_bag = bag
-      locker.empty = false
-      locker.save
-      ticket = Ticket.new(locker_id: locker.id, bag_id: bag.id)
-      p bag
-      p locker
-      p ticket
-      expect(Bag.count).to be(count + 1)
+    context 'with invalid attributes' do
+      it 'does not save the new bag in the database'
+      it 're-renders the :new template'
     end
-    # it 'redirects user to the /bags/:id #show route' do
-    # response = post(:create, bag: { size: bag.size })
-    # p response
-    # expect(response).to redirect_to("/bags/#{bag.id}")
-    # end
   end
 end
