fc496c7c4970ccdaaa26eaba3a94ab5bc91b1f71
Matthew Villwock <mtvillwock@gmail.com>
2015-04-17 18:12:53 -0700

    refactor bag_spec.rb to use factory girl, 3 tests passing

--- spec/models/bag_spec.rb
+++ spec/models/bag_spec.rb
@@ -2,26 +2,21 @@ require 'spec_helper'
 
 describe Bag do
   context 'valid Bag' do
-    it 'is valid with a size "small"' do
+    it 'is valid with a valid size "small", "medium", or "large" provided' do
       # bag = Bag.new(size: "small")
       expect(build(:bag)).to be_valid
     end
-
-    # it 'is valid with a size "medium"' do
-    #   bag = Bag.new(size: "medium")
-    #   expect(bag).to be_valid
-    # end
-
-    # it 'is valid with a size "large"' do
-    #   bag = Bag.new(size: "large")
-    #   expect(bag).to be_valid
-    # end
   end
 
   context 'invalid Bag' do
     it 'is invalid without a size' do
-      bag = Bag.new(size: nil)
-      expect(bag).to_not be_valid
+      expect(build(:bag, size: nil)).to_not be_valid
+    end
+
+    it 'is invalid with an improper size' do
+      bag = Bag.new(size: "not one of the possible sizes")
+      bag.valid?
+      expect(bag.errors.messages[:size]).to include("please use small, medium, or large as values")
     end
   end
 end
