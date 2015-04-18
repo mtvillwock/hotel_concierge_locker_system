4cc6882668b7e9ec5cae95c9f6157b7928289df4
Matthew Villwock <mtvillwock@gmail.com>
2015-04-18 02:09:49 -0700

    add has_one associations to transaction.rb

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,42 +1,42 @@
 class TransactionsController < ApplicationController
   def index
-    @transactions = Transaction.all
+    #@transactions = Transaction.all
   end
 
   def show
-    @transaction = Transaction.find_by(id: params[:id])
+    #@transaction = Transaction.find_by(id: params[:id])
   end
 
   def new
-    @transaction = Transaction.new
+    #@transaction = Transaction.new
   end
 
   def create
-      @transaction = Transaction.new(locker_id: params[:locker_id], bag_id: params[:bag_id], ticket_id: params[:ticket_id])
-    if @transaction.save
-      render json: { success: "transaction ##{@transaction.id} created"}
-    else
-      render json: { error: "transaction not created"}
-    end
+    # @transaction = Transaction.new(locker_id: params[:locker_id], bag_id: params[:bag_id], ticket_id: params[:ticket_id])
+    # if @transaction.save
+    #   render json: { success: "transaction ##{@transaction.id} created"}
+    # else
+    #   render json: { error: "transaction not created"}
+    # end
   end
 
   def edit
-    @transaction = Transaction.find(params[:id])
+    # @transaction = Transaction.find(params[:id])
   end
 
   def update
-    @transaction = Transaction.find(params[:id])
-    @transaction.update_attributes(params[:transaction])
-    # have ajax send hash of { transaction: { locker_id: lockerId, bag_id: bagId, ticket_id: ticketId}}
-    if @transaction.save
-      render json: { success: "transaction ##{@transaction.id} updated"}
-    else
-      render json: { error: "transaction failed to update"}
-    end
+    # @transaction = Transaction.find(params[:id])
+    # @transaction.update_attributes(params[:transaction])
+    # # have ajax send hash of { transaction: { locker_id: lockerId, bag_id: bagId, ticket_id: ticketId}}
+    # if @transaction.save
+    #   render json: { success: "transaction ##{@transaction.id} updated"}
+    # else
+    #   render json: { error: "transaction failed to update"}
+    # end
   end
 
   def destroy
-    @transaction = Transaction.find_by(id: params[:id])
-    @transaction.destroy
+    # @transaction = Transaction.find_by(id: params[:id])
+    # @transaction.destroy
   end
 end
