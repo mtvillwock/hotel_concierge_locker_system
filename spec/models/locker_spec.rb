e1bcae02e34cc8a68ebb17877c7fbd86c32d43db
Matthew Villwock <mtvillwock@gmail.com>
2015-04-17 18:51:02 -0700

    refactor locker tests using factory girl syntax, add validation error test, 6 tests passing

--- spec/models/locker_spec.rb
+++ spec/models/locker_spec.rb
@@ -7,25 +7,27 @@ describe Locker do
 
   context 'valid Locker' do
     it 'is valid with a size "small"' do
-      locker = Locker.new(size: "small")
-      expect(locker).to be_valid
+      expect(build(:small_locker)).to be_valid
     end
 
     it 'is valid with a size "medium"' do
-      locker = Locker.new(size: "medium")
-      expect(locker).to be_valid
+      expect(build(:medium_locker)).to be_valid
     end
 
     it 'is valid with a size "large"' do
-      locker = Locker.new(size: "large")
-      expect(locker).to be_valid
+      expect(build(:large_locker)).to be_valid
     end
   end
 
   context 'invalid Locker' do
     it 'is invalid without a bag size' do
-      locker = Locker.new(size: nil)
-      expect(locker).to be_invalid
+      expect(build(:locker, size: nil)).to be_invalid
+    end
+
+    it 'is invalid with an improper size' do
+      locker = Locker.new(size: "not one of the possible sizes")
+      locker.valid?
+      expect(locker.errors.messages[:size]).to include("please use small, medium, or large as values")
     end
   end
 end
