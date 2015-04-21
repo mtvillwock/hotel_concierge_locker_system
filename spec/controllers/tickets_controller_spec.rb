17f8c892ede68256d46c2552d0abd95eea1e3fe7
Matthew Villwock <mtvillwock@gmail.com>
2015-04-20 23:14:44 -0700

    index tests for rendering and assignment passing, delete test passing

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -3,61 +3,50 @@ require 'factory_girl_rails'
 
 RSpec.describe TicketsController, type: :controller do
   describe "GET #index" do
-    it "has a 200 status code" do
+    it 'populates an ActiveRecord collection of Tickets' do
+      tickets = Ticket.all
       get :index
-      expect(response.status).to eq(200)
+      expect(assigns(:tickets)).to match(tickets)
+    end
+    it 'renders the :index template' do
+      get :index
+      expect(response).to render_template :index
     end
   end
-
-  describe 'GET #show' do
-    it 'assigns the requested Ticket to @ticket'
-    it 'renders the :show template'
-  end
-
-  describe 'GET #new' do
-    it 'assigns a new Ticket to @ticket'
-    it 'renders the :new template'
-  end
-
-  # describe 'GET #edit' do
-  #   xit 'assigns the requested Ticket to @ticket'
-  #   xit 'renders the :edit template'
-  # end
 
   describe 'POST #create' do
     context "with valid attributes" do
       it 'saves the new Ticket in the database' do
-        # Is there a proper way to extract this into multiple tests?
-
-        # ticket = Ticket.new(locker_id: , bag_id: )
-        # ticket.save
-      end
-
-      it "redirects to tickets#show" do
-        # post :create, ticket: { locker_id: , bag_id: }
-        # expect(response).to redirect_to(assigns(:ticket))
-        # expect(response.status).to eq(302)
-        # this is returning 200
+        bag = create(:small_bag)
+        locker = create(:small_locker)
+        valid_attributes = { locker_id: locker.id, bag_id: bag.id }
+        ticket_count = Ticket.count
+        expect{
+          post :create, ticket: valid_attributes
+        }.to change{Ticket.count}.by(1)
       end
     end
     context 'with invalid attributes' do
-      it 'does not save the new ticket in the database'
-      it 're-renders the :new template'
+      it 'does not save the new ticket in the database' do
+        bag = create(:small_bag)
+        locker = create(:small_locker)
+        invalid_attributes = { locker_id: "foo", bag_id: "bar" }
+        expect{
+          post :create, ticket: invalid_attributes
+        }.to_not change{Ticket.count}
+      end
     end
   end
 
-  # describe 'PATCH #update' do
-  #   context "with valid attributes" do
-  #     it "updates the Ticket status" # this shouldn't be a route
-  #     it "redirects to the Ticket"
-  #   end
-  #   context "with invalid attributes" do
-  #     it "does not update the Ticket"
-  #     it "re-renders the :edit template"
-  #   end
-  # end
   describe "DELETE #destroy" do
-    it "deletes the Ticket from the database"
-    it "redirects to tickets#index"
+    it "deletes the Ticket from the database" do
+      bag = create(:small_bag)
+      locker = create(:small_locker)
+      valid_attributes = { locker_id: locker.id, bag_id: bag.id }
+      ticket = Ticket.create! valid_attributes
+      expect {
+        delete :destroy, {:id => ticket.id}
+      }.to change(Ticket, :count).by(-1)
+    end
   end
 end
